0a47a0590104b4e7557a0bba6861df3d16d4f350
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-01 17:52:22 +0200

    part3: ex 3.5

--- index.js
+++ index.js
@@ -45,6 +45,25 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random()*100000)
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
