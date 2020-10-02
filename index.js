fdaa8d0948ea3478bbedcf9f221841a300c90798
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-02 11:17:01 +0200

    part3: ex 3.14

--- index.js
+++ index.js
@@ -68,20 +68,22 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
+    /* 
     if (persons.find(person => person.name === body.name)) {
         return res.status(400).json({
             error: 'name must be unique'
         })
-    }
+    } 
+    */
     
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: Math.floor(Math.random()*100000)
-    }
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
