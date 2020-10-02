0847732aa67e2358bdc690c82deffbb05bdbf095
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2020-10-02 11:59:10 +0200

    part3: ex 3.18

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ let persons = [
 ]
 
 app.get('/info', (req, res) => {
-    res.send(`Phonebook has info for ${persons.length} people<br/><br/>${new Date()}`)
+    Person.countDocuments({}, (err, c) => {
+        res.send(`Phonebook has info for ${c} people<br/><br/>${new Date()}`)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
